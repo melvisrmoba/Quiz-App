14aa5239fd74dcddca93dcac0486cd737d92ca29
melvisrmoba <melvis.r.moba@gmail.com>
2026-06-10 12:30:02 -0500

    start screen message modified

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,7 +1,7 @@
 export default function StartScreen({ onStart }) {
   return (
     <div className="start-screen">
-      <h1>Trivia Quiz</h1>
+      <h1>Quizz Me </h1>
       <p>Let's get started on a simple quiz that will test you</p>
       <button onClick={onStart}>Start Quiz</button>
     </div>
